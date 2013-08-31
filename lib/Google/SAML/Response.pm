8d15a2a069bf2190726903ac3cce037a244bd575
Manni Heumann <heumann@strato-rz.de>
2013-08-31 15:39:41 +0200

    hmm. cpan already has a version 12. set new version number to .13

--- lib/Google/SAML/Response.pm
+++ lib/Google/SAML/Response.pm
@@ -13,7 +13,7 @@ Google's SSO implementation
 
 =head1 VERSION
 
-You are currently reading the documentation for version 0.12
+You are currently reading the documentation for version 0.13
 
 =head1 DESCRIPTION
 
@@ -115,7 +115,7 @@ use Carp;
 use HTML::Entities;
 
 
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 
 =head2 new
 
